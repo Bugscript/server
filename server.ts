9abe9b7c55dde551d15334bddb07174e974a0935
Bugscript <34430540+Bugscript@users.noreply.github.com>
2018-03-29 17:08:30 -0500

    Create server.ts

---
+++ server.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import WebSocket from "ws";
+import fs from "fs";
+
+const app = express();
+const server = WebSocket.Server({
+	port: 8088
+});
+
+app.get("/", (req, res) => {
+	res.send("hello world");
+});
+
+server.on("connection", (client) => {
+
+	client.on("message", (data) => {
+		client.send(`Hello from server.`);
+	});
+});
+
+app.listen(PORT, () => {
+	console.log(`Listening on port: ${8080}`);
+});
